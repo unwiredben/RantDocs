d420096924aaee149e02e3d171f566d02eb58234
Nicholas Fleck <grenzatuna@gmail.com>
2014-12-22 17:32:26 -0600

    Update prism-rant.js

--- assets/js/prism-rant.js
+++ assets/js/prism-rant.js
@@ -1,11 +1,31 @@
+var rantModes = [
+	"lower", "upper", "sentence", "word", "title", "none",
+	"normal", "roman(?:[-_](?:upper|lower))?", "group(?:[-_](?:commas|dots))?", "verbal[-_]en",
+	"locked", "c?deck", "ordered", "reverse",
+	"public", "private", "internal",
+	"different", "(?:not[-_])?equal", "less", "greater", "none", "one", "all", "any"
+].join("|");
+
+var rantFunctions = [
+	"[rs](?:ep)?", "n", "num", "after", "alt", "any", "arg", "before", "branch", "b", "break", "capsinfer", "case", "caps",
+	"chance", "char", "close", "clrt", "cmp", "define", "dist", "else", "even", "extern", "(?:not)?first", "g", "generation",
+	"get", "group", "if[n]?def", "is", "(?:not)?last", "len", "merge", "m", "(?:not)?middle", "nth", "numfmt", "mark", "match",
+	"odd", "osend", "out", "repcount", "rc", "repindex", "ri", "repnum", "rn", "send", "src", "step", "undef", "x", "xnew",
+	"xnone", "xpin", "xreset", "xseed", "xunpin"
+	].join("|");
+
 Prism.languages.rant = {
 	'escaped': {
-		pattern: /([^\\]|^)".+?"/g,
-		alias: 'punctuation',
+		pattern: /\\((?:\d+,)?(?:[^u\s\r\n]|u[0-9a-f]{4}))/,
+		alias: 'punctuation'
+	},
+	'constliteral': {
+		pattern: /(^|[^\\])("(?:(?:[^"]|"")*)?")/,
+		alias: 'string',
 		lookbehind: true
 	},
 	'comment': {
-		pattern: /\s*#.*?(?=[\r\n]|$)/g
+		pattern: /#.*/
 	},
 	'equation': {
 		pattern: /(``|`)[\s\S]+?\1/g,
@@ -25,11 +45,17 @@ Prism.languages.rant = {
 		}
 	},
 	'function': {
-		pattern: /(\[)(after|alt|any|arg|before|branch|break|capsinfer|case|chance|char|close|clrt|cmp|define|dist|else|even|extern|first|g|get|group|ifdef|ifndef|is|last|len|merge|middle|notfirst|notlast|notmiddle|nth|num|numfmt|mark|match|odd|osend|out|rep|repcount|repindex|repnum|send|sep|src|step|undef|x|xnew|xnone|xpin|xreset|xseed|xunpin)\b/gi,
+		pattern: new RegExp("((?:^|[^\\\\])\\[)([$]\\w+|" + rantFunctions + ")(?:[:\\]])", "i"),
 		lookbehind: true
 	},
+	'modename': {
+		pattern: new RegExp("([:;]\\s*)(" + rantModes + ")\\s*(?=[;\\]])", "i"),
+		alias: 'keyword',
+		lookbehind: true
+	}
 	'list': {
-		pattern: /\[%[=:][\w;\+]+?\]/g,
-		alias: 'entity'
+		pattern: /((?:^|[^\\])\[)(%[:=!]?\w+)(\s*(?:\+\^?\%?|\=\@?|![@^$]?|\$\^?|\@[?^$]?))?/,
+		alias: 'entity',
+		lookbehind: true
 	}
 }
